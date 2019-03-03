e40b20609305969ae8386d4d3ba9533b005903df
J-falconetti <Joseph.falconetti@gmail.com>
2019-03-03 07:48:12 -0500

    html about finished css stated JS/JQ sets up randon mubers for crystals

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # unit-4-game
 crystal game
+game that picks a random number and the player needs to pick from 4 crystals with random values: to win game user must get to total without going over.  All values will rest after game is either won or lost and total score will be displayed values are noted in JS/html file(s) as needed
--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,1 +1,25 @@
 // this is the Javascript page
+// game that picks a random number and the player needs to pick from 4 crystals with random values: to win game user must get to total without going over.  All values will rest after game is either won or lost and total score will be displayed
+
+
+// setting up varibles Total, Crystals 1 to 4, crystal cunter, win and loss counters
+
+var total;
+var wins=0;
+var loss=0;
+
+//generating random values
+    // Total is for total number between 19 and 120
+ var total = [Math.floor(Math.random() * (120 - 19))];
+    // getting values for the 4 crystals vad cr[x]
+    for (i = 1; i < 5; i++) {
+        var gemval = Math.floor(Math.random() * 12);
+        console.log(gemval)
+        var gems = $("<div>");
+        $(".gem").append(gems)
+    }
+    console.log(total);
+
+
+
+
