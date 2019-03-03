1cca32099b8eea11b065e7ff2ad48d52255687f0
J-falconetti <Joseph.falconetti@gmail.com>
2019-03-03 16:31:44 -0500

    total for clicks working, jumbotron displays all  needed info

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -5,26 +5,45 @@
 
 // setting up varibles Total, Crystals 1 to 4, crystal cunter, win and loss counters
 
-var total;
-var wins=0;
-var loss=0;
+var total = 0
+var clicks = 0
+var wins = 0;
+var loss = 0;
 
-//generating random values
+
+        //generating random values
     // Total is for total number between 19 and 120
- total =Math.floor(Math.random() * 101) + 19;
- $("#total").html("Your Total Goal is: " +total)
+    total = Math.floor(Math.random() * 101) + 19;
+    $("#total").html("Your Total Goal is: " + total + " You have " + wins + " wins and " + loss + " losses")
+    $("#totclick").html("Your Clicks = " +clicks)
     // getting values for the 4 crystals and placing cards 
     for (i = 1; i < 5; i++) {
 
-        var genran = Math.floor(Math.random() * 12) 
+        var genran = Math.floor(Math.random() * 11) + 1
         var gems = $("<div>");
-            gems.attr({
+        gems.attr({
             "class": 'gems',
             "data-genran": genran
-    });
-
+        });
+    
         $(".playcard").append(gems);
     }
-$(".gems").on('click', function () {
-   
-});
+
+    $(".gems").on('click', function () {
+        var tc = parseInt($(this).attr("data-genran"));
+
+        clicks += tc;
+
+        $("#totclick").html("Your Clicks = " +clicks)
+        if (clicks === total) {
+            alert("You won!");
+            wins++;
+            
+        }
+        else if (clicks > total) {
+            alert("you loss");
+            loss++;
+        
+        }
+
+    });
