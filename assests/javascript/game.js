f553035a475fe541f567b11c13d5f37d2b48ac17
J-falconetti <Joseph.falconetti@gmail.com>
2019-03-04 22:27:57 -0500

    Ok I was wrong still not 100%

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -9,67 +9,70 @@ var total = 0;
 var clicks = 0;
 var wins = 0;
 var loss = 0;
+
+// Total is for total number between 19 and 120
 total = Math.floor(Math.random() * 101) + 20;
 $("#total").html("Your Total Goal is: " + total + " You have " + wins + " wins and " + loss + " losses")
-$("#totclick").html("Your Clicks = " + clicks)
+$("#totclick").html("Your Clicks = " +clicks)
+
 //create reset function
 function sgr() {
 
-    total = 0
-    clicks = 0
-    //generating random values
+    total=0
+    clicks=0
+           //generating random values
     // Total is for total number between 19 and 120
     total = Math.floor(Math.random() * 101) + 20;
     $("#total").html("Your Total Goal is: " + total + " You have " + wins + " wins and " + loss + " losses")
-    $("#totclick").html("Your Clicks = " + clicks)
+    $("#totclick").html("Your Clicks = " +clicks)
 }
+ 
 
 
+    // getting values for the 4 crystals and placing cards 
+    for (i = 0; i < 4; i++) {
 
-// getting values for the 4 crystals and placing cards 
-for (i = 0; i < 4; i++) {
+        var genran = Math.floor(Math.random() * 11) + 1
+        // var gems = $("<div>");
+        //create render img via loop
+        var photo = ["./assests/images/gem1.jpg","./assests/images/gem2.jpg","./assests/images/gem3.jpg","./assests/images/gem4.jpg"];
+        var gems = $("<img alt='image'>");
+        gems.attr({
+            "class": 'gems',
+            "data-genran": genran,
+            "src": photo[i]
 
-    var genran = Math.floor(Math.random() * 11) + 1
-    // var gems = $("<div>");
-    //create render img via loop
-    var photo = ["./assests/images/gem1.jpg", "./assests/images/gem2.jpg", "./assests/images/gem3.jpg", "./assests/images/gem4.jpg"];
-    var gems = $("<img alt='image'>");
-    gems.attr({
-        "class": 'gems',
-        "data-genran": genran,
-        "src": photo[i]
-
-    });
-
-    $(".playcard").append(gems);
-    // $(gems).append(pic)
-
-}
+        });
+    
+        $(".playcard").append(gems);
+        // $(gems).append(pic)
+        
+    }
 
 // diplaying gems
-$(".gems").on('click', function () {
-    var tc = parseInt($(this).attr("data-genran"));
+    $(".gems").on('click', function () {
+        var tc = parseInt($(this).attr("data-genran"));
 
-    clicks += tc;
-    // checking win/loss
-    $("#totclick").html("Your Clicks = " + clicks)
-    if (clicks === total) {
-        alert("You won!");
-        wins++;
-        sgr();
+        clicks += tc;
+// checking win/loss
+        $("#totclick").html("Your Clicks = " +clicks)
+        if (clicks === total) {
+            alert("You won!");
+            wins++;
+            sgr();
+            
+            
+            
+        }
+        else if (clicks > total) {
+            alert("you loss");
+            loss++;
+            sgr();
+            
+            
+        
+        }
 
 
 
-    }
-    else if (clicks > total) {
-        alert("you loss");
-        loss++;
-        sgr();
-
-
-
-    }
-
-
-
-});
+    });
